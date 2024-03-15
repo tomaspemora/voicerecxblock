936d5e70e8c7f4001e49979666055ca01820f92a
Thomas Peet <tomaspemora@gmail.com>
2024-03-15 00:09:51 -0300

    fix unicode python 2 function

--- voicerecognizer/utils.py
+++ voicerecognizer/utils.py
@@ -3,8 +3,11 @@ import pkg_resources
 from django.template import Context, Template
 
 def load_resource(resource_path):
-    resource_content = resource_content = pkg_resources.resource_string(__name__, resource_path)
-    return unicode(resource_content)
+    # Load the resource content as binary data
+    resource_content = pkg_resources.resource_string(__name__, resource_path)
+    # Decode the binary data to a text string using UTF-8 or another appropriate encoding
+    return resource_content.decode('utf-8')
+
 
 def render_template(template_path, context={}):
     template_str = load_resource(template_path)
